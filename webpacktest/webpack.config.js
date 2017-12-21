8368e960336d21f3a9431266b493e91f8afedb5a
Stream <Stream@DESKTOP-H7F2K8C>
2017-12-21 17:00:05 +0800

    webpack

--- webpacktest/webpack.config.js
+++ webpacktest/webpack.config.js
@@ -1,19 +1,35 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+ const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
+
 module.exports = {
-	entry:{
-		app:"./src/index.js",
-		print:"./src/print.js"
+  	entry: {
+  		app: './src/index.js'
 	},
-	output:{
-		filename:"[name].bundle.js",
-		path:path.resolve(__dirname,"dist")
+	devtool: 'inline-source-map',
+	devServer: {
+  		contentBase: './dist',
+  		hot: true
 	},
-	plugins:[
-		new CleanWebpackPlugin('dist'),
-		new HtmlWebpackPlugin({
-			title:"Output Management"
-		})
-	]
-}
+	module: {
+		rules: [
+			{
+	        	test: /\.css$/,
+				use: ['style-loader', 'css-loader']
+			}
+		]
+	},
+	plugins: [
+      new CleanWebpackPlugin(['dist']),
+      new HtmlWebpackPlugin({
+        title: 'Hot Module Replacement'
+      }),
+      new webpack.NamedModulesPlugin(),
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'dist')
+    }
+};
